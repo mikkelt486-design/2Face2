37c488bac0137139e3f21cda6ea0827ed871dc23
mikkelt486-design <mikkelt486@gmail.com>
2026-06-04 23:48:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+require("dotenv").config();
+
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+  ],
+});
+
 client.once("ready", () => {
   console.log(`2Face is online as ${client.user.tag}`);
 });
+
+client.login(process.env.TOKEN);
